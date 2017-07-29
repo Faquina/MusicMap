b249a6fb8d6f25909afd748d2e5dbdb1fcf2b073
Brian Craig <briancraigok@gmail.com>
2017-07-29 16:37:11 -0300

    agregado el login mock

---
+++ app/src/main/java/com/sadsadsad/musicmap/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.sadsadsad.musicmap;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void goToMain(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
